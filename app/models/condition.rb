526151a0b31c7ef532a49d6b970496f7ae57a340
Scott Wade <scwade@yahoo.com>
2009-07-01 14:07:15 -0700

    tweaks2

--- app/models/condition.rb
+++ app/models/condition.rb
@@ -4,9 +4,6 @@ class Condition < ActiveRecord::Base
   def self.find_my_condition
     find(:all, :order => "name")
   end
-
-#	validates_presence_of :pmd_patient_no
-  validates_numericality_of :pmd_patient_no
   
 #  Validation not currently applicable
 #  validate :fieled_must_be_higher_than_zero
