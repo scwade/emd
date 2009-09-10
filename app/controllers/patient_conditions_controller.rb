649ff7f9b94b78b8d095b2eb7f2327a71836eac4
Scott Wade <scwade@yahoo.com>
2009-09-10 10:09:50 -0700

    added rollands CSS to patient profiles - New only.  It needs work

--- app/controllers/patient_conditions_controller.rb
+++ app/controllers/patient_conditions_controller.rb
@@ -5,18 +5,36 @@ class PatientConditionsController < ApplicationController
     @patient_conditions = PatientCondition.all
     @conditions = Condition.find(:all)
     @patient_profiles = PatientProfile.find(:all)
-
-
-  def find_condition_name(id)
-    @conditions = Condition.find(:all)
-    @patient_profiles = PatientProfile.find(:all)
-    Condition.find(:all, :order => "name")
-    return id.name
-  end
     
     respond_to do |format|
       format.html # index.html.erb
       format.xml  { render :xml => @patient_conditions }
+    end
+  end
+
+  # GET /patient_conditions/list
+  def list
+    @patient_conditions = PatientCondition.all
+    @conditions = Condition.find(:all)
+    @patient_profiles = PatientProfile.find(:all)
+    
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @patient_conditions }
+    end
+  end
+
+
+  # GET /patient_conditions/1
+  # GET /patient_conditions/1.xml
+  def find_condition_name(id)
+    @conditions = Condition.find(:all)
+    @patient_profiles = PatientProfile.find(:all)
+    Condition.find(:all, :order => "name")
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml => @patient_condition }
     end
   end
 
