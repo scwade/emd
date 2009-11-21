ba5aeeb14bc7a61f79904209a813661baa4bd10c
rolland <hoyt94587@yahoo.com>
2009-11-21 07:27:08 -0800

    small changes, emd.css bacground changed

--- db/migrate/20091119174512_add_active_inactive_to_patient_profile.rb
+++ db/migrate/20091119174512_add_active_inactive_to_patient_profile.rb
@@ -1,6 +1,6 @@
 class AddActiveInactiveToPatientProfile < ActiveRecord::Migration
   def self.up
-    add_column :patient_profiles, :active_patient, :boolean, :default => false, :null => false
+    add_column :patient_profiles, :active_patient, :boolean, :default => true, :null => false
   end
 
   def self.down
